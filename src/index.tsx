a8f9058ed645bf99cba7c1cef81724679470449f
wangjianhui16 <wangjianhui3@jd.com>
2021-06-17 00:47:22 +0800

    init: init repo & update README.md

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
-// export function sayHello(name: string) {
-//   return `Hey ${name}, say hello to TypeScript.`;
-// }
-
 import React from "react";
 
-const SayHello = ({ name }: { name: string }): JSX.Element => (
+const SayHelloWorld = ({ name }: { name: string }): JSX.Element => (
   <div>Hey {name}, say hello to TypeScript.</div>
 );
 
-export default SayHello;
+export default SayHelloWorld;
